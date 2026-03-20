b9d1319f289e59c05fe6e6b6ee1ace46a716f78b
SilasAngare1 <silasangare04@hotmail.com>
2026-03-20 12:48:27 -0300

    exercicio apostila 4

--- apostila04/src/br/com/fiap/loja/view/Terminal.java
+++ apostila04/src/br/com/fiap/loja/view/Terminal.java
@@ -2,6 +2,7 @@ package br.com.fiap.loja.view;
 
 import br.com.fiap.loja.model.Produto;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Terminal {
@@ -13,18 +14,26 @@ public class Terminal {
         Produto whey = new Produto();
 
         System.out.println("digite o nome do produto:");
-
         whey.nome = leitor.nextLine();
+        System.out.println("digite o preco:");
+        whey.preco = leitor.nextDouble();
+        System.out.println("digite a quantidade:");
+        whey.quantidade = leitor.nextInt();
+        System.out.println("diga se tem garantia (true or false):");
+        whey.garantia = leitor.nextBoolean();
+        System.out.println("digite o tipo do whey");
+        whey.tipo = leitor.nextLine();
 
+        whey.preco *= whey.quantidade;
 
-
-
+        System.out.println(whey.nome);
+        System.out.println(whey.quantidade);
+        System.out.println(whey.preco);
+        System.out.println(whey.garantia);
+        System.out.println(whey.tipo);
 
 
     }
-
-
-
 
 
 
